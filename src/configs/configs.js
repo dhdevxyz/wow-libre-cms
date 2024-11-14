dd7d9fc29df4f438c54569037076cc15c7e30dc0
Sebastian Chitiva <mschitiva68@ucatolica.edu.co>
2024-11-14 10:09:01 -0500

    update

--- src/configs/configs.js
+++ src/configs/configs.js
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://15.204.134.74:8080/core' : 'https://4db3-181-51-34-195.ngrok-free.app/core';
 export const BASE_URL_TRANSACTION = process.env.NODE_ENV === 'production' ? 'http://15.204.134.74:8080/transaction' : 'https://4db3-181-51-34-195.ngrok-free.app/transaction';
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://15.204.134.74:8080/core' : 'https://4db3-181-51-34-195.ngrok-free.app/core';
