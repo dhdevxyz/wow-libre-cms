b3082c1d2bd228caf3d267f1ee07bccd2d1dcb4b
Sebastian Chitiva <mschitiva68@ucatolica.edu.co>
2024-11-27 13:13:32 -0500

    corrijo redirecionamiento aws balancer

--- src/configs/configs.js
+++ src/configs/configs.js
@@ -1,2 +1,2 @@
-export const BASE_URL_TRANSACTION = process.env.NODE_ENV === 'production' ? 'https://wowlibre-external-176154737.us-east-2.elb.amazonaws.com:8092/transaction' : 'http://localhost:8092/transaction';
-export const BASE_URL = process.env.NODE_ENV === 'production' ?'https://wowlibre-external-176154737.us-east-2.elb.amazonaws.com:8091/core': 'http://localhost:8091/core';
+export const BASE_URL_TRANSACTION = process.env.NODE_ENV === 'production' ? 'https://wowlibre-external-176154737.us-east-2.elb.amazonaws.com/transaction' : 'http://localhost:8092/transaction';
+export const BASE_URL = process.env.NODE_ENV === 'production' ?'https://wowlibre-external-176154737.us-east-2.elb.amazonaws.com/core': 'http://localhost:8091/core';
