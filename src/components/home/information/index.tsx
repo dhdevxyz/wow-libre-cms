ed43bccbcc7b9c27896c57d6270aa1308d0bfe14
Sebastian Chitiva <manuchitivar@gmail.com>
2024-12-28 13:26:50 -0500

    Ajusto lo de la home

--- src/components/home/information/index.tsx
+++ src/components/home/information/index.tsx
@@ -59,13 +59,25 @@ const Information = () => {
             </div>
           </div>
         </div>
-        <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+        <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden flex flex-col items-center">
           <img
             className="w-[48rem] max-w-none rounded-xl bg-gray-800 shadow-xl ring-1 ring-gray-600/10 sm:w-[57rem]"
-            src="https://static.wixstatic.com/media/5dd8a0_cc65edad0cce497c924b91d4d298ac33~mv2.png"
+            src="https://static.wixstatic.com/media/5dd8a0_878828466ac34fb5b9f833d34adf8a29~mv2.webp"
             alt="img-beta-server"
           />
+          <button
+            onClick={() =>
+              window.open(
+                "https://drive.usercontent.google.com/download?id=1jH5dirVKluY_L1kJwNicCd2A0ccjSvec&export=download&authuser=0",
+                "_blank"
+              )
+            }
+            className="text-2xl mt-8 inline-flex items-center justify-center rounded-lg bg-gradient-to-r from-yellow-400 via-orange-500 to-red-500 px-20 py-6 font-semibold text-white shadow-sm hover:from-yellow-500 hover:via-orange-600 hover:to-red-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+          >
+            Download Game
+          </button>
         </div>
+
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
             <div className="max-w-xl text-base/7 text-gray-300 lg:max-w-lg">
