7359e1b7cc9573a88705fb0d3b1e306b90e38db5
Sebastian Chitiva <manuchitivar@gmail.com>
2025-05-11 22:18:17 -0500

    Correcion del nombre

--- src/constants/configs.ts
+++ src/constants/configs.ts
@@ -1,5 +1,5 @@
 export const webProps ={
-  serverName: "Tu Server",
+  serverName: "Entropiux",
   logo:"https://static.wixstatic.com/media/5dd8a0_4a5bff42a39c47c2ae67d5dde07455f5~mv2.webp",
   loginBanner: "https://static.wixstatic.com/media/5dd8a0_36549a48c1674682a9c8c8264decd5a2~mv2.webp",
 };
