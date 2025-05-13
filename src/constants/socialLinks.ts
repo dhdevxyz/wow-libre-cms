8dc9ffef76c7b2f77dd69615285aaea2d1141ee1
Sebastian Chitiva <manuchitivar@gmail.com>
2025-05-13 13:54:28 -0500

    Cambio apuntamiento de redes sociales

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -1,25 +1,25 @@
 export const socialLinks = [
     {
       name: "Facebook",
-      href: "https://www.facebook.com/WowLibre/",
+      href: "https://www.facebook.com/people/EntropiuX-Wow/61574837116481/#",
       icon: "../img/footer/facebook.webp",
       alt: "Facebook Icon",
     },
     {
       name: "Instagram",
-      href: "https://www.tiktok.com/@wowlibre?lang=es",
+      href: "https://www.facebook.com/people/EntropiuX-Wow/61574837116481/#",
       icon: "../img/footer/instagram.webp",
       alt: "Instagram Icon",
     },
     {
       name: "WhatsApp",
-      href: "https://chat.whatsapp.com/BDELJKhuJkWIMKxF8ExIdN",
+      href: "https://www.facebook.com/people/EntropiuX-Wow/61574837116481/#",
       icon: "../img/footer/whatsapp.webp",
       alt: "WhatsApp Icon",
     },
     {
       name: "Telegram",
-      href: "https://t.me/wowlibreservers",
+      href: "https://www.facebook.com/people/EntropiuX-Wow/61574837116481/#",
       icon: "../img/footer/telegram.webp",
       alt: "Telegram Icon",
     },
