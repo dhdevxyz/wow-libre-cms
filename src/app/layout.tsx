3f171b7c15be965d4aeae884cec92a8151e36327
Sebastian Chitiva <manuchitivar@gmail.com>
2025-05-13 21:58:39 -0500

    Ajusto la metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,17 +15,17 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: webProps.serverName,
   description:
-    "Administrative web platform with advanced tools to optimize management and enhance your community's experience.",
+    "EntropiuX is the first private server for The War Within — get ahead of the game and explore Azeroth like never before.",
   openGraph: {
     title: webProps.serverName,
     description:
-      "Administrative web platform with advanced tools to optimize management and enhance your community's experience.",
+      "EntropiuX is the first private server for The War Within — get ahead of the game and explore Azeroth like never before.",
     images: [
       {
-        url: "https://static.wixstatic.com/media/5dd8a0_cc65edad0cce497c924b91d4d298ac33~mv2.png",
+        url: "https://static.wixstatic.com/media/5dd8a0_b4a3d979f15148ad8819296bc2781816~mv2.jpg",
         width: 1200,
         height: 630,
-        alt: "WowLibre Plataform",
+        alt: "EntropiuX Plataform",
         type: "image/png",
       },
     ],
@@ -34,9 +34,10 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: webProps.serverName,
     description:
-      "Administrative web platform with advanced tools to optimize management and enhance your community's experience.",
+      "EntropiuX is the first private server for The War Within — get ahead of the game and explore Azeroth like never before.",
+    creator: "@entropiux",
     images: [
-      "https://static.wixstatic.com/media/5dd8a0_cc65edad0cce497c924b91d4d298ac33~mv2.png",
+      "https://static.wixstatic.com/media/5dd8a0_b4a3d979f15148ad8819296bc2781816~mv2.jpg",
     ],
   },
 };
